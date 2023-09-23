488b337e6bb551499cacec4a9d2bf5f01318aad6
AnjanaManipuzha <anjanamanipuzha@gmail.com>
2023-09-23 20:07:42 +0530

    initial commit

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Typical from "react-typical";
+import './Profile.css';
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="#">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-google-plus-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Ethidu</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Ethusiastic Dev ",
+                    1000,
+                    "Fullstack Dev",
+                    1000,
+                    "Mern Stack Dev",
+                    1000,
+                    "Cross platform Dev",
+                    1000,
+                    "React/native Dev",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications withfront and backend operatons
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href="ehizcv.pdf" download="ehidu ehizcv.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
